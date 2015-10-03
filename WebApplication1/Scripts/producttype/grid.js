bec3bab051dbe7c8c9c4afa9c965e72d2fabbd07
maximumz <cssskrumax@hotmail.com>
2015-10-03 19:32:28 +0700

    update Scripts

--- WebApplication1/Scripts/producttype/grid.js
+++ WebApplication1/Scripts/producttype/grid.js
@@ -1,36 +1,36 @@
 ﻿ProductType.Grid = function () {
     this.grid = $('#grid_productType');
-    this.pager = '#pager_productType';
-    this.producttypeModal = $('#producttype_modal');
-    this.deletetypeModal = $('#deletetype_modal');
-    this.getUrl = '';
-    this.createUrl = '';
-    this.editUrl = '';
-    this.deleteUrl = '';
-    this.form = $('#producttype_form');
-    this.deletetypeForm = $('#deletetype_form');
-    this.rowData = null;
+    //this.pager = '#pager_productType';
+    //this.producttypeModal = $('#producttype_modal');
+    //this.deletetypeModal = $('#deletetype_modal');
+    //this.getUrl = '';
+    //this.createUrl = '';
+    //this.editUrl = '';
+    //this.deleteUrl = '';
+    //this.form = $('#producttype_form');
+    //this.deletetypeForm = $('#deletetype_form');
+    //this.rowData = null;
 };
 
 ProductType.Grid.prototype.init = function () {
     this.createGrid();
     var me = this;
-    var processJson = function (data) {
-        console.log('create Url :' + me.deleteUrl)
-        me.producttypeModal.modal('hide');
-        me.deletetypeModal.modal('hide');
-        me.grid.trigger('reloadGrid');
-    };
+    //var processJson = function (data) {
+    //    console.log('create Url :' + me.deleteUrl)
+    //    me.producttypeModal.modal('hide');
+    //    me.deletetypeModal.modal('hide');
+    //    me.grid.trigger('reloadGrid');
+    //};
 
-    this.form.ajaxForm({
-        dataType: 'json',
-        success: processJson
-    });
+    //this.form.ajaxForm({
+    //    dataType: 'json',
+    //    success: processJson
+    //});
 
-    this.deletetypeForm.ajaxForm({
-        dataType: 'json',
-        success: processJson
-    });
+    //this.deletetypeForm.ajaxForm({
+    //    dataType: 'json',
+    //    success: processJson
+    //});
 };
 
 ProductType.Grid.prototype.createGrid = function () {
@@ -50,38 +50,38 @@ ProductType.Grid.prototype.createGrid = function () {
         rowNum: 10,
         rowList: [10, 20, 30],
 
-        onSelectRow: function (id) {
-            me.rowData = me.grid.jqGrid('getRowData', id);
-            console.log(me.rowData);
-        },
+        //onSelectRow: function (id) {
+        //    me.rowData = me.grid.jqGrid('getRowData', id);
+        //    console.log(me.rowData);
+        //},
 
         pager: me.pager,
         autowidth: true,
-        height: 400,
-        caption: 'รายการประเภทสินค้า'
+        //height: 400,
+        //caption: 'รายการประเภทสินค้า'
     });
 
-    this.grid.jqGrid('filterToolbar', { beforeSearch: true })
-    this.grid.jqGrid('navGrid', this.pager, {
-        edit: true, add: true, del: true, refresh: true, search: false,
-        addfunc: function () {
-            console.log('create Url :' + me.createUrl);
-            me.form.attr('action', me.createUrl);
-            me.form.clearForm();
-            me.producttypeModal.modal('show');
-        },
-        editfunc: function () {
-            console.log('create Url :' + me.editUrl);
-            me.form.attr('action', me.editUrl);
-            me.form.clearForm();
-            FormHelper.setFormValue(me.rowData, me.form);
-            me.producttypeModal.modal('show');
-        },
-        delfunc: function () {
-            console.log('create Url :' + me.deleteUrl);
-            me.deletetypeForm.attr('action', me.deleteUrl);
-            FormHelper.setFormValue(me.rowData, me.deletetypeForm);
-            me.deletetypeModal.modal('show');
-        }
-    });   
+    //this.grid.jqGrid('filterToolbar', { beforeSearch: true })
+    //this.grid.jqGrid('navGrid', this.pager, {
+    //    edit: true, add: true, del: true, refresh: true, search: false,
+    //    addfunc: function () {
+    //        console.log('create Url :' + me.createUrl);
+    //        me.form.attr('action', me.createUrl);
+    //        me.form.clearForm();
+    //        me.producttypeModal.modal('show');
+    //    },
+    //    editfunc: function () {
+    //        console.log('create Url :' + me.editUrl);
+    //        me.form.attr('action', me.editUrl);
+    //        me.form.clearForm();
+    //        FormHelper.setFormValue(me.rowData, me.form);
+    //        me.producttypeModal.modal('show');
+    //    },
+    //    delfunc: function () {
+    //        console.log('create Url :' + me.deleteUrl);
+    //        me.deletetypeForm.attr('action', me.deleteUrl);
+    //        FormHelper.setFormValue(me.rowData, me.deletetypeForm);
+    //        me.deletetypeModal.modal('show');
+    //    }
+    //});   
 };
